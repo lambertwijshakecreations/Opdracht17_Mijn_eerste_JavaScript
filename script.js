d20d7501f5f4b200ad7c7d412b93042eb7b33c6a
lambertwijshakecreations <73013216+lambertwijshakecreations@users.noreply.github.com>
2021-01-17 14:24:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,43 @@
+//opdracht #4
+console.log("Hello Winc Academy");
+
+//opdracht #5
+let name = 'Lambert';
+console.log("Lambert");
+
+//opdracht #6 : let if; Dit is niet mogelijk omdat dit een gereserveerde naam is die bijvoorbeeld voor Boolean wordt gebruikt.
+
+//opdracht #7
+console.log(4+4); //Dit is een som.
+
+let number1 = '4';
+let number2 = '4';
+console.log(number1 + number2);//dit niet hier worden beide getallen naast elkaar geplaatst.
+
+//opdracht #8
+console.log(10+10);
+console.log(5-4);
+console.log(4*6);
+console.log(10/2);
+console.log(10%50);
+
+//opdracht #9
+let age = 38;
+console.log(age);
+typeof age;
+
+/* opdracht #10
+let age = achtendertig;
+console.log(age);
+typeof age;
+
+Deze werkt niet omdat ik niet begrijp wat er van mij gevraagd wordt. 
+als ik dit doe wrodt er een error gegeven*/
+
+
+
+
+
+
+
+
